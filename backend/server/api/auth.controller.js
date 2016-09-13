efa14400f89cffa01b8f418cad832e290c8ec13d
Gaston Robledo <gastonrobledo@gmail.com>
2016-09-13 16:50:41 -0300

    adding changes to auth controller

--- backend/server/api/auth.controller.js
+++ backend/server/api/auth.controller.js
@@ -5,25 +5,28 @@ var Players = mongoose.model('Players');
 
 exports.auth = function(req,res){
 
-    Players.findOne({"email": req.body.email}).lean().then(function(player){
+    Players.findOne({"email": req.body.email}).then(function(player){
         if(player) {
-            var valid = Players.verifyPasswordSync(req.body.password);
-            if (valid) {
-                //search for user then create token
-                var token = jwt.sign(player, config.secretKey, {
-                    expiresIn: '24h' // expires in 24 hours
-                });
-
-                res.json({
-                    success: true,
-                    token: token
-                });
-            } else {
-                res.send(400).json({
-                    success: false,
-                    message: "Email / Password invalid"
-                });
-            }
+            player.verifyPassword(req.body.password, function(err, valid){
+                if (valid) {
+                    //search for user then create token
+                    console.log(valid, config.secretKey);
+                    jwt.sign(player, config.secretKey, {
+                        expiresIn: '24h' // expires in 24 hours
+                    }, function(err, token){
+                        console.log(err, token);
+                        res.json({
+                            success: true,
+                            token: token
+                        });
+                    });
+                } else {
+                    res.send(400).json({
+                        success: false,
+                        message: "Email / Password invalid"
+                    });
+                }
+            });
         }else{
             res.status(400).json({
                 success: false,
