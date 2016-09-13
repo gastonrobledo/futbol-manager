b3ae475a12542be0c0cebb24843846b915cd82eb
Gaston Robledo <gastonrobledo@gmail.com>
2016-09-13 16:25:46 -0300

    upgrade changes on config env

--- backend/server/config/env/production.js
+++ backend/server/config/env/production.js
@@ -3,5 +3,5 @@
 var path = require('path');
 
 module.exports = {
-    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/futbol-manager',
+    db: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/futbol-manager',
 };
